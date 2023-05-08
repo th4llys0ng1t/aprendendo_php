0983470e9a813ced5f62fdffa3350f0cee7498cf
Thallyson <thallysonvidal566@gmail.com>
2023-05-08 16:29:13 -0300

    ajeitando algumas coisas

--- aula01_curriculo/atividade_02/Atividade_media.php
+++ aula01_curriculo/atividade_02/Atividade_media.php
@@ -2,15 +2,46 @@
 
     $msg = "";
     $result = "";
+    $nota1 = "";
+    $nota2 = "";
+    $nota3 = "";
+    $nota4 = "";
+    $media = "";
+    $botao = "";
 
+    if(isset($_POST["calculo"])){
+        $botao = $_POST["calculo"];
 
+        if(isset($_POST["n1"])){
+            $nota1 = $_POST["n1"];
+        }
 
+        if(isset($_POST["n2"])){
+            $nota1 = $_POST["n2"];
 
+        }
+        if(isset($_POST["n3"])){
+            $nota1 = $_POST["n3"];
 
+        }
+        if(isset($_POST["n4"])){
+            $nota1 = $_POST["n4"];
 
+        }
 
+        if($_POST["tipo_media"] == "aritmetica"){
+            $media = mediaAritmetica($nota1, $nota2, $nota3, $nota4);
+        }
 
+    }
 
+    function mediaAritmetica($nota1, $nota2, $nota3, $nota4){
+
+        $mediaArit = "";
+        $mediaArit = ($nota1 + $nota2 + $nota3 + $nota4)/4;
+        return $mediaArit;
+
+    }
 
 ?>
 
@@ -36,38 +67,36 @@
         </h4>
         
         <p>
-            <input type="radio" name="tipo_media" value="aritimetica">Aritimética <br>
+            <input type="radio" name="tipo_media" value="aritmetica" checked>Aritmética <br>
             <input type="radio" name="tipo_media" value="ponderada">Ponderada <br>
             <input type="radio" name="tipo_media" value="harmonica">Harmônica
         </p>
 
         <p>
             <label for="n1">Nota 1:</label> 
-            <input type="text" name="n1">
+            <input type="number" name="n1">
         </p>
 
         <p>
             <label for="n2">Nota 2:</label> 
-            <input type="text" name="n2">
+            <input type="number" name="n2">
         </p>
 
         <p>
             <label for="n3">Nota 3:</label>
-            <input type="text" name="n3">
+            <input type="number" name="n3">
         </p>
 
         <p>
             <label for="n4">Nota 4:</label>
-            <input type="text" name="n4">
+            <input type="number" name="n4">
         </p>
 
-        <p><input type="submit" value="Calcule"></p>
+        <p><input type="submit" value="Calcule" name="calculo"></p>
 
         <h3>Resultado: <?php echo $result ?></h3>
- 
-    
+
     </form>
-
-
+    
 </body>
 </html>
