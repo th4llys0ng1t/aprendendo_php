f5fb13ec0ed424f00d937f488eb71f079875658a
Thallyson <thallysonvidal566@gmail.com>
2023-05-08 15:58:54 -0300

    atividade da média

---
+++ aula01_curriculo/atividade_02/Atividade_media.php
@@ -0,0 +1,73 @@
+<?php 
+
+    $msg = "";
+    $result = "";
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcule sua média IFRN</title>
+</head>
+<body>
+    
+    <form action="Atividade_media.php" method="post">
+
+        <h1>Calcule sua média IFRN</h1>
+
+        <h3>Mensagens: <?php echo $msg ?></h3>
+
+        <h4>
+            <label for="tipo_media">Escolha o tipo de média 
+            antes de calcular:</label>
+        </h4>
+        
+        <p>
+            <input type="radio" name="tipo_media" value="aritimetica">Aritimética <br>
+            <input type="radio" name="tipo_media" value="ponderada">Ponderada <br>
+            <input type="radio" name="tipo_media" value="harmonica">Harmônica
+        </p>
+
+        <p>
+            <label for="n1">Nota 1:</label> 
+            <input type="text" name="n1">
+        </p>
+
+        <p>
+            <label for="n2">Nota 2:</label> 
+            <input type="text" name="n2">
+        </p>
+
+        <p>
+            <label for="n3">Nota 3:</label>
+            <input type="text" name="n3">
+        </p>
+
+        <p>
+            <label for="n4">Nota 4:</label>
+            <input type="text" name="n4">
+        </p>
+
+        <p><input type="submit" value="Calcule"></p>
+
+        <h3>Resultado: <?php echo $result ?></h3>
+ 
+    
+    </form>
+
+
+</body>
+</html>
