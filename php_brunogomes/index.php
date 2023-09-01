2ea98c2df7401c599f5e75ed92e3607908f41b4b
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-09-01 16:12:24 -0300

    começando com o melhor professor

---
+++ php_brunogomes/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <?php 
+        echo "<p>Olá mundo</p>";
+
+        //comentário de uma linha
+
+        /* comentários 
+            de 
+            várias 
+            linhas
+         */
+
+         $idade = 30;
+         $nota = 10.5;
+         $instituto = "IFRN Campus Canguaretama";
+         $aprovado = TRUE;
+     ?>   
+</body>
+</html>
