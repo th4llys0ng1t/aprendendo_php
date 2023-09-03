6e291a1d37af5ab0607f3ac9067acf133cbcf91d
Thallyson Kauã <thallysonvidal566@gmail.com>
2023-09-03 08:46:20 -0300

    aos pouquinhos

---
+++ php_brunogomes/lista_exercicios/questao02.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 2</title>
+</head>
+<body>
+    <?php 
+        $instituto = "Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte";
+        echo "<p>". $instituto ."</p>";
+    ?> 
+</body>
+</html>
--- php_brunogomes/lista_exercicios/questao04.php
+++ php_brunogomes/lista_exercicios/questao04.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Questão 1</title>
+    <title>Questão 4</title>
 </head>
 <body>
-    <?php echo "<p>É preciso fazer todos os algoritmos</p>" ?>
-    <?php echo "<p>para aprender PHP!</p>" ?>
+    <?php 
+        $num = 30;
+        $dobro = $num*2;
+
+        echo "O número salvo foi " . $num . " e o seu dobro é " . $dobro;
+    ?>   
 </body>
 </html>
