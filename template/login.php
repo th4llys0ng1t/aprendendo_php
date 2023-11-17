da2ae65f57165f8240e443819ca4e559f36eeee4
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-11-17 15:08:18 -0300

    adicionando o template

---
+++ template/login.php
@@ -0,0 +1,12 @@
+<form action="" method="post">
+    <p>
+        <label for="usuario">Usuário:</label>
+        <input type="text" name="usuario" id="usuario">
+    </p>
+    <p>
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" id="senha">
+    </p>
+
+    <p><input type="submit" value="Cadastrar"></p>
+</form>
---
+++ template/script.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS cadastro;
+
+USE cadastro;
+
+CREATE TABLE IF NOT EXISTS usuario(
+    id int not null auto_increment primary key,
+    nome varchar(100) not null,
+    usuario varchar(50) not null,
+    senha char(40) not null,
+);
+
+insert into usuario (usuario, senha) values ("bruno", "123");
